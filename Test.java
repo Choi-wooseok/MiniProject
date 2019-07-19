d3e72f57b783ac559fd4dde574ab7b7eb722a2ae
Choi-wooseok <52450245+Choi-wooseok@users.noreply.github.com>
2019-07-19 18:05:03 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Test {
+
+public static void main(String[] args) {
+//		Test t = new Test();
+//		t.service();
+		
+		new BoardUI().service();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
